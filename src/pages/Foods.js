86da9c381414ba08605c006516866e85621b3c23
Vinicius <xxvi.paris@gmail.com>
2022-07-14 17:37:28 -0300

    add pagina de login

---
+++ src/pages/Foods.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Foods() {
+  return <h1>Foods</h1>;
+}
---
+++ src/pages/Login.js
@@ -0,0 +1,44 @@
+import React, { useContext, useEffect } from 'react';
+
+import MyContext from '../context/myContext';
+
+function Login() {
+  const { login: { email, senha },
+    handleChange, isDisabled, handleDisabled, handleSubmit } = useContext(MyContext);
+  useEffect(handleDisabled, [email, senha]);
+  return (
+    <form onSubmit={ handleSubmit }>
+      <label htmlFor="email">
+        E-mail:
+        <input
+          id="email"
+          type="text"
+          data-testid="email-input"
+          placeholder="exemplo@exemplo.com"
+          value={ email }
+          onChange={ handleChange }
+        />
+      </label>
+      <label htmlFor="senha">
+        Senha:
+        <input
+          id="senha"
+          type="password"
+          data-testid="password-input"
+          placeholder="Mínimo de 6 caracteres"
+          value={ senha }
+          onChange={ handleChange }
+        />
+      </label>
+      <button
+        type="submit"
+        data-testid="login-submit-btn"
+        disabled={ isDisabled }
+      >
+        Enter
+      </button>
+    </form>
+  );
+}
+
+export default Login;
