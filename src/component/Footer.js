de943ea54ed357ab0ed31374c7f73efcf177ed7d
Willianson Dantas <williansondantas@gmail.com>
2022-07-15 16:34:04 -0300

    requisitos-16-17-18

---
+++ src/component/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+
+function Footer() {
+  return (
+    <footer
+      data-testid="footer"
+    >
+      <Link
+        to="/drinks"
+      >
+        <img
+          src={ drinkIcon }
+          alt="icone de bebida"
+          data-testid="drinks-bottom-btn"
+        />
+      </Link>
+      <Link
+        to="/foods"
+      >
+        <img
+          src={ mealIcon }
+          alt="ícone de comida"
+          data-testid="food-bottom-btn"
+        />
+      </Link>
+    </footer>
+  );
+}
+
+export default Footer;
