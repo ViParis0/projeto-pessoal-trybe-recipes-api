a4757cac4ecc98c5491906cd392ed392fd053a5b
Vinicius <xxvi.paris@gmail.com>
2022-07-15 18:11:25 -0300

    iniciando req 14

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,16 +1,36 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../component/Header';
 import MyContext from '../context/myContext';
 
 export default function Drinks() {
+  const history = useHistory();
+  const [showDrinks, setShowDrinks] = useState(false);
+  const [limitedDrinks, setLimitedDrinks] = useState([]);
   const {
     setGetPage,
+    getDrinks,
   } = useContext(MyContext);
   useEffect(() => setGetPage('drinks'), []);
+
+  useEffect(() => {
+    if (getDrinks.length === 1) {
+      history.push(`/drinks/${getDrinks[0].idDrink}`);
+    } else {
+      const LIMIT_OF_ARR = 12;
+      const limitedArr = getDrinks.slice(0, LIMIT_OF_ARR);
+      setLimitedDrinks(limitedArr);
+      setShowDrinks(true);
+    }
+  }, [getDrinks]);
   return (
     <>
       <Header pageName="Drinks" />
-      <h1>Drinks</h1>
+      {showDrinks && limitedDrinks.map((drink) => (
+        <div key={ drink.idDrink }>
+          <p>{drink.strDrink}</p>
+        </div>
+      ))}
     </>
   );
 }
--- src/pages/Foods.js
+++ src/pages/Foods.js
@@ -1,15 +1,36 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../component/Header';
 import MyContext from '../context/myContext';
 
 export default function Foods() {
+  const history = useHistory();
+  const [showMeals, setShowMeals] = useState(false);
+  const [limitedMeals, setLimitedMeals] = useState([]);
   const {
     setGetPage,
+    getMeals,
   } = useContext(MyContext);
   useEffect(() => setGetPage('foods'), []);
+
+  useEffect(() => {
+    if (getMeals.length === 1) {
+      history.push(`/foods/${getMeals[0].idMeal}`);
+    } else {
+      const LIMIT_OF_ARR = 12;
+      const limitedArr = getMeals.slice(0, LIMIT_OF_ARR);
+      setLimitedMeals(limitedArr);
+      setShowMeals(true);
+    }
+  }, [getMeals]);
   return (
     <div>
       <Header pageName="Foods" />
+      {showMeals && limitedMeals.map((drink) => (
+        <div key={ drink.idMeal }>
+          <p>{drink.strMeal}</p>
+        </div>
+      ))}
     </div>
   );
 }
